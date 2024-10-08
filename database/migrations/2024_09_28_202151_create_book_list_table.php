37d5944cc7fbd673d59fd93016eb06b3d4929653
JorikDura <vandarkslave@outlook.com>
2024-10-08 05:14:50 +0300

    new indexes

--- database/migrations/2024_09_28_202151_create_book_list_table.php
+++ database/migrations/2024_09_28_202151_create_book_list_table.php
@@ -13,8 +13,8 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::create('user_book_list', function (Blueprint $table) {
-            $table->foreignIdFor(Book::class);
-            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Book::class)->index();
+            $table->foreignIdFor(User::class)->index();
             $table->enum('status', array_column(BookListStatus::cases(), 'value'))
                 ->default(BookListStatus::PLANNED);
             $table->boolean('is_favorite')->default(false);
